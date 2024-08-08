2e0ac5ad3ec2cbde2cc6f96a2e825436ae385061
Ahmethan1 <ahmethansafa@hotmail.com>
2024-08-08 15:02:56 +0300

    examService Added

---
+++ examService/src/test/java/com/turkcell/gyt/examService/ExamServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turkcell.gyt.examService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
